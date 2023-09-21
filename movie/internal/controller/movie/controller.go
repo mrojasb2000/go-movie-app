012323958a2aa092c0a47a6be20543fa9208e175
mavro <mrojasb2000@gmail.com>
2023-09-21 17:11:00 -0300

    chore: add movie struct and service

---
+++ movie/internal/controller/movie/controller.go
@@ -0,0 +1,57 @@
+package movie
+
+import (
+	"context"
+	"errors"
+
+	metadatamodel "github.com/mrojasb2000/go-movie-app/metadata/pkg/model"
+	"github.com/mrojasb2000/go-movie-app/movie/internal/gateway"
+	"github.com/mrojasb2000/go-movie-app/movie/pkg/model"
+	ratingmodel "github.com/mrojasb2000/go-movie-app/rating/pkg/model"
+)
+
+// ErrNotFound is returned when the movie metadata is not found.
+var ErrNotFound = errors.New("movie metadata not found")
+
+type ratingGateway interface {
+	GetAggregatedRating(ctx context.Context, recordID ratingmodel.RecordID, recordType ratingmodel.RecordType) (float64, error)
+	PutRating(ctx context.Context, recordID ratingmodel.RecordID, recordType ratingmodel.RecordType, raitng *ratingmodel.Rating) error
+}
+
+type metadataGateway interface {
+	Get(ctx context.Context, id string) (*metadatamodel.Metadata, error)
+}
+
+// Controller defines a movie service controller.
+type Controller struct {
+	ratingGateway   ratingGateway
+	metadataGateway metadataGateway
+}
+
+// New creates a new movie service controller.
+func New(ratingGateway ratingGateway, metadataGateway metadataGateway) *Controller {
+	return &Controller{ratingGateway, metadataGateway}
+}
+
+// Get returns the movie details including the aggrerated rating and movie metadata.
+func (c *Controller) Get(ctx context.Context, id string) (*model.MovieDetails, error) {
+	metadata, err := c.metadataGateway.Get(ctx, id)
+	if err != nil && errors.Is(err, ErrNotFound) {
+		return nil, ErrNotFound
+	} else if err != nil {
+		return nil, err
+	}
+	details := &model.MovieDetails{
+		Metadata: *metadata,
+	}
+	rating, err := c.ratingGateway.GetAggregatedRating(ctx, ratingmodel.RecordID(id), ratingmodel.RecordTypeMovie)
+	if err != nil && errors.Is(err, gateway.ErrNotFound) {
+		// Just proceed in this case, it's ok not have ratings yet.
+	} else if err != nil {
+		return nil, err
+	} else {
+		details.Rating = &rating
+	}
+	return details, nil
+
+}
